731496dc0613cc75cac4a6bc3a28fec79fc875b1
Hawker <62062830+BlckHawker@users.noreply.github.com>
2023-08-15 16:16:30 -0400

    created model draft

--- Assets/Kuro/Scripts/Kuro.cs
+++ Assets/Kuro/Scripts/Kuro.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using KModkit;
 using Rnd = UnityEngine.Random;
 
-public class Template : MonoBehaviour {
+public class Kuro : MonoBehaviour {
 
    public KMBombInfo Bomb;
    public KMAudio Audio;
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person {
+    public string Name { get; private set; }
+    public string Id { get; private set; }
+
+    public Person(string name, string id) 
+    {
+        Name = name;
+        Id = id;
+    }
+}
