92e8cfb70629bb4f8534ee66f932dcb7634b2649
Hawker <62062830+BlckHawker@users.noreply.github.com>
2023-08-16 06:50:00 -0400

    Made kuro pfp random

--- Assets/Kuro/Scripts/Kuro.cs
+++ Assets/Kuro/Scripts/Kuro.cs
@@ -9,32 +9,74 @@ using Rnd = UnityEngine.Random;
 
 public class Kuro : MonoBehaviour {
 
-   public KMBombInfo Bomb;
-   public KMAudio Audio;
+    //todo create a class of the voice channels
+    //todo create the text channels objects
+    //todo assign people to the chill vc randomly on start
+    //todo fix kuro pfps looking the opposite way
+
+    private TextChannel generalTextChannel;
+    private TextChannel modIdeasTextChannel;
+    private TextChannel repoRequestTextChannel;
+    private TextChannel voiceTextModdedTextChannel;
+
+    public Material[] kuroMoods;
+    private Material currentKuroMood;
+
+   private KMBombInfo Bomb;
+   private KMAudio Audio;
 
    static int ModuleIdCounter = 1;
    int ModuleId;
    private bool ModuleSolved;
 
-   void Awake () {
-      ModuleId = ModuleIdCounter++;
-      /*
-      foreach (KMSelectable object in keypad) {
-          object.OnInteract += delegate () { keypadPress(object); return false; };
-      }
-      */
+    void Awake() {
 
-      //button.OnInteract += delegate () { buttonPress(); return false; };
+        Bomb = GetComponent<KMBombInfo>();
+        Audio = GetComponent<KMAudio>();
+        ModuleId = ModuleIdCounter++;
 
-   }
+        //changing kuro pfp
+        GameObject textChannels = transform.Find("Text Channels").gameObject;
+        MeshRenderer kuroPfp = transform.Find("Profile").Find("PFP").GetComponent<MeshRenderer>();
+        currentKuroMood = kuroMoods[Rnd.Range(0, kuroMoods.Length)];
+        kuroPfp.material = currentKuroMood;
 
-   void Start () {
+        //setting text channels
+        generalTextChannel = CreateTextChannel(textChannels.transform.Find("general").gameObject);
+        modIdeasTextChannel = CreateTextChannel(textChannels.transform.Find("mod ideas").gameObject);
+        repoRequestTextChannel = CreateTextChannel(textChannels.transform.Find("repo request").gameObject);
+        voiceTextModdedTextChannel = CreateTextChannel(textChannels.transform.Find("voice text modded").gameObject);
+        generalTextChannel.Activate();
+        modIdeasTextChannel.Deactivate();
+        repoRequestTextChannel.Deactivate();
+        voiceTextModdedTextChannel.Deactivate();
+
+
+        /*
+        foreach (KMSelectable object in keypad) {
+            object.OnInteract += delegate () { keypadPress(object); return false; };
+        }
+        */
+
+        //button.OnInteract += delegate () { buttonPress(); return false; };
+
+    }
+
+    void Start () {
 
    }
 
    void Update () {
 
    }
+
+    public TextChannel CreateTextChannel(GameObject gameObject)
+    {
+        GameObject highlight = gameObject.transform.Find("highlight").gameObject;
+        TextMesh textMesh = gameObject.transform.Find("label").GetComponent<TextMesh>();
+
+        return new TextChannel(textMesh, highlight);
+    }
 
 #pragma warning disable 414
    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
---
+++ Assets/Kuro/Scripts/Text Channel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextChannel {
+    private static Color grayTextColor = new Color(148f/255, 155f/255, 164f/255);
+    private TextMesh textMesh;
+    private GameObject highlight;
+
+    public TextChannel(TextMesh textMesh, GameObject highlight)
+    {
+        this.textMesh = textMesh;
+        this.highlight = highlight;
+    }   
+
+    /// <summary>
+    /// Sets the color of the text to white and turns on the highlight
+    /// </summary>
+    public void Activate()
+    {
+        textMesh.color = Color.white;
+        highlight.SetActive(true);
+    }
+
+    /// <summary>
+    /// Sets the color of the text to gray and turns off the highlight
+    /// </summary>
+    public void Deactivate()
+    {
+        textMesh.color = grayTextColor;
+        highlight.SetActive(false);
+    }
+
+}
